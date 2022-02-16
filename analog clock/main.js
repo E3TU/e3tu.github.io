d81e23ab4ac6d9ca49d046bd4e99639d97cb152d
E3TU <eetux22@protonmail.com>
2022-02-16 11:10:44 +0200

    kommentteja css ja javascriptille

--- analog clock/main.js
+++ analog clock/main.js
@@ -12,7 +12,7 @@ const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
 //Array kuukausille
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
 
-//Setinterval päivittää kellon sekuntin välein jotta sekuntti viisari liikkuu
+//SetInterval päivittää kellon sekuntin välein jotta sekuntti viisari liikkuu
 setInterval(() => {
     const time = new Date();
     const day = time.getDay();
@@ -24,17 +24,15 @@ setInterval(() => {
     const all = hour + ":" + minutes;
     const all2 = days[day] + " " + date + " " + months[month];
     const minutesUnder10 = hour + ":" + 0 + minutes;
-    
+    //Output date ja time elementteihin
     dateEl.innerText = all2;
     timeEl.innerText = all;
 
-    test = hour + ":" + minutes;
-
-    //Jos minuutit on alle kymmenen lisäämme nolla minuutin eteen
+    //Jos minuutit on alle kymmenen lisäämme nollan minuutin eteen
     if (minutes < 10){
         timeEl.innerText = minutesUnder10;
     }
-    
+    //Liikutetaan viisareita
     hoursEl.style.transform = `translate(-50%, -100%) rotate(${hour * 30}deg)`
 
     minutesEl.style.transform = `translate(-50%, -100%) rotate(${minutes * 6}deg)`
@@ -50,6 +48,7 @@ theme.addEventListener("click", () => {
         document.body.style.backgroundColor = "#191919";
         document.getElementById("date").style.color = "white";
         document.getElementById("clock").style.borderColor = "white";
+        document.getElementById("clock").style("clockwhite");
         theme.innerHTML = "Light mode";
     } else{
         html.classList.add("dark");
